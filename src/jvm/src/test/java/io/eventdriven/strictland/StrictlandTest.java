a5b49326f0dbec13787cbdba687356a43a07dbec
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-06-04 13:57:29 +0200

    Added intellij setup and devcontainer action

--- src/jvm/src/test/java/io/eventdriven/strictland/StrictlandTest.java
+++ src/jvm/src/test/java/io/eventdriven/strictland/StrictlandTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class StrictlandTest {
+final class StrictlandTest {
 
     @Test
     void greet_returnsExpectedMessage() {
