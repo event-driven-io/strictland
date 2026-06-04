b74c3cf21485dfd4b970c4d09373623f287ed3b1
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-06-04 13:14:38 +0200

    Added formatting and static analysis

--- src/jvm/src/test/java/io/eventdriven/strictland/StrictlandTest.java
+++ src/jvm/src/test/java/io/eventdriven/strictland/StrictlandTest.java
@@ -1,8 +1,8 @@
 package io.eventdriven.strictland;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class StrictlandTest {
 
