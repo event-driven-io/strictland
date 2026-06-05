be473256db426e5051f6e9e34f6db522110b6e19
Oskar Dudycz <oskar.dudycz@gmail.com>
2026-06-05 13:32:30 +0200

    Added vscode workspace

--- src/jvm/src/test/java/io/eventdriven/strictland/StrictlandTest.java
+++ src/jvm/src/test/java/io/eventdriven/strictland/StrictlandTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 final class StrictlandTest {
-
     @Test
     void greet_returnsExpectedMessage() {
         var strictland = new Strictland();
+
         assertEquals("Hello from Strictland!", strictland.greet());
     }
 }
